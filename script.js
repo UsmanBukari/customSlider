008bfb0c39148ddbf1648039e5d103b67e0f7b27
UsmanBuk <usmanbukhari541@gmail.com>
2024-11-03 20:44:20 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+  
